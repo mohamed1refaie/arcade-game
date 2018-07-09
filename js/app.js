c1b4466935aa6670301884cfeebf6566e283158c
Mohamed Refaie <mohamed1refaie@hotmail.com>
2018-07-09 22:36:53 +0200

    Make Enemies  Move

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
-// Enemies our player must avoid
+class Enemy {
+    constructor(x=0,y=50,speed=256){
+        this.x=x;
+        this.y=y;
+        this.speed=speed;
+        this.sprite='images/enemy-bug.png';
+    }
+    update(dt){
+        this.x+=this.speed*dt;
+        if(this.x>=505)
+            this.x=0;
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+/*// Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -6,20 +23,22 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
+    this.x=5;
+    this.y=5;
+};*/
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+/*Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+};*/
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+/*Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+};*/
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -44,3 +63,9 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+let allEnemies=[];
+for(let i=0;i<10;i++) {
+    let enemy=new Enemy(0,Math.random()*184+50,Math.random()*256);
+    allEnemies.push(enemy);
+}
